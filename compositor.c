c0b2716eda7136424786aaae793aa0b4d0ed3a05
john smith <tj@tjv>
2012-04-14 20:27:52 -0500

    working stuff

--- compositor.c
+++ compositor.c
@@ -1,6 +1,11 @@
 
+//#define LAYERSIZE 2048
+//#define LAYERSIZE 4096
+//#define LAYERSIZE 1024
 #define LAYERSIZE 2048
 #define NUMLAYERS 16
+#define NUMBRUSHES 10
+#define PI 3.1415926
 
 typedef struct
 {
@@ -8,9 +13,31 @@ typedef struct
 	float opacity;
 } layer_t;
 
+typedef struct
+{
+	GLuint texid;
+	float r,g,b,a;
+	float size;
+	char softedge;
+} brush_t;
+
+
 static layer_t layers[NUMLAYERS];
 static GLuint fbo;
+static int currentlayer = 1;
+static float windoww, windowh, windowa;
 
+static brush_t brushes[NUMBRUSHES];
+static int selectedbrush = 0;
+
+static float zoom = 1;
+static float panx = 0, pany = 0;//TODO:implement this
+static float rotation = PI/4;
+
+
+static GLuint wheeltexture;
+static int renderwheel = 0;
+static float wheelx, wheely;
 
 static void target(GLuint texid)
 {
@@ -24,33 +51,61 @@ static void target(GLuint texid)
 static void untarget()
 {
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);//switch to default fbo
-	glViewport(0,0,800,600);//TODO:make these right
+	glViewport(0,0,windoww,windowh);
 }
 
 static void drawlayer(int ind)
 {
 	glBindTexture(GL_TEXTURE_2D, layers[ind].texid);
+	glPushMatrix();
+	glRotatef(45,0,0,1);
 	glBegin(GL_QUADS);
 	glColor4f(1,1,1,layers[ind].opacity);
 //	printf("op %d %f\n", ind, layers[ind].opacity);
-	glTexCoord2f(0,0);glVertex3f(-1,-1,0);
-	glTexCoord2f(0,1);glVertex3f(-1,1,0);
-	glTexCoord2f(1,1);glVertex3f(1,1,0);
-	glTexCoord2f(1,0);glVertex3f(1,-1,0);
+	float xt, yt;
+	glTexCoord2f(0,0);glVertex3f(-1*windowa*zoom+panx,-1*zoom+pany,0);
+	glTexCoord2f(0,1);glVertex3f(-1*windowa*zoom+panx,1*zoom+pany,0);
+	glTexCoord2f(1,1);glVertex3f(1*windowa*zoom+panx,1*zoom+pany,0);
+	glTexCoord2f(1,0);glVertex3f(1*windowa*zoom+panx,-1*zoom+pany,0);
 	glEnd();
+	glPopMatrix();
+}
+static void drawwheel()
+{
+	glBindTexture(GL_TEXTURE_2D, wheeltexture);
+	glBegin(GL_TRIANGLE_FAN);
+	glColor4f(1,1,1,1);
+	glTexCoord2f(0,0);
+	glVertex3f(-0.8*windowa+wheelx,-0.8+wheely,0);
+	glTexCoord2f(1,0);
+	glVertex3f(0.8*windowa+wheelx,-0.8+wheely,0);
+	glTexCoord2f(1,1);
+	glVertex3f(0.8*windowa+wheelx,0.8+wheely,0);
+	glTexCoord2f(0,1);
+	glVertex3f(-0.8*windowa+wheelx,0.8+wheely,0);
+	glEnd();
+
+}
+static void wtos(float *x, float *y)
+{
+	if(x != NULL)
+		*x = ((*x-0.5)*2/windowa)/zoom-panx/windowa/zoom;
+	if(y!= NULL)
+		*y = ((*y-0.5)*-2)/zoom-pany/zoom;
 }
 void c_init()
 {
 	int i, j;
 	glEnable(GL_TEXTURE_2D);
 	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE);
 	for(i=0;i<NUMLAYERS;i++)
 	{
 		glGenTextures(1,&layers[i].texid);
 		glBindTexture(GL_TEXTURE_2D, layers[i].texid);
 		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
 		layers[i].opacity = 0;
 	//	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, LAYERSIZE, LAYERSIZE, 0, GL_RGBA, GL_FLOAT, NULL);
 	}
@@ -58,47 +113,212 @@ void c_init()
 	glGenFramebuffers(1,&fbo);
 	//make some more textures and shit here
 //TODO:after this to be removed
-	glBindTexture(GL_TEXTURE_2D, layers[0].texid);
+/*	glBindTexture(GL_TEXTURE_2D, layers[0].texid);
 	static float serpi[LAYERSIZE][LAYERSIZE][4];//causes stack overflow if not static
 	for(i=0;i<LAYERSIZE;i++)
 	{
 		for(j=0;j<LAYERSIZE;j++)
 		{
-			serpi[i][j][0] = (j&i) == 0;
+			serpi[i][j][0] = (j+i<LAYERSIZE)?((j&i) == 0):(((LAYERSIZE-j)&(LAYERSIZE-i)) == 0);
 			serpi[i][j][3] = 1;
 		}
 	}
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, LAYERSIZE, LAYERSIZE, 0, GL_RGBA, GL_FLOAT, serpi);
-	layers[0].opacity = 0.5;
+	layers[0].opacity = 1;
 
 	for(i=0;i<LAYERSIZE;i++)
 	{
 		for(j=0;j<LAYERSIZE;j++)
 		{
 			serpi[i][j][0]=0;
-			serpi[i][j][1] = ((-j)&i)*0.0001;
-			serpi[i][j][3] = ((-j)&i)*0.0001;
+			serpi[i][j][3]=serpi[i][j][1] = fabs(sin(i*0.1)+cos(j*0.1))*0.5;
 		}
 	}
 	glBindTexture(GL_TEXTURE_2D, layers[1].texid);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, LAYERSIZE, LAYERSIZE, 0, GL_RGBA, GL_FLOAT, serpi);
 	layers[1].opacity = 1;
 	
+*/
+	static float serpi[LAYERSIZE][LAYERSIZE][4];//causes stack overflow if not static
+	glBindTexture(GL_TEXTURE_2D, layers[1].texid);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, LAYERSIZE, LAYERSIZE, 0, GL_RGBA, GL_FLOAT, serpi);
+	layers[1].opacity = 1;
+	glBindTexture(GL_TEXTURE_2D, layers[0].texid);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, LAYERSIZE, LAYERSIZE, 0, GL_RGBA, GL_FLOAT, serpi);
+	layers[0].opacity = 1;
+//initialize wheel texture
+	static float serpiw[512][512][4];//causes stack overflow if not static
+	glGenTextures(1,&wheeltexture);
+	glBindTexture(GL_TEXTURE_2D, wheeltexture);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+	static float reds[] = {1,1,0,0,0,1,1,  1};
+	static float greens[] = {0,1,1,1,0,0,0,  0};
+	static float blues[] = {0,0,0,1,1,1,0,  0};
+	for(i=0;i<512;i++)
+	{
+		for(j=0;j<512;j++)
+		{
+			serpiw[i][j][0] =serpiw[i][j][1]=serpiw[i][j][2]=serpiw[i][j][3]=0;
+			if((i-256)*(i-256)+(j-256)*(j-256) <256*256 && (i-256)*(i-256)+(j-256)*(j-256) > 50*50)
+			{
+				float angle = atan((float)(i-256)/(j-256));
+				if(j < 256) angle = angle+PI;
+				angle += PI/2;
+				serpiw[i][j][3] = 1;
+				int index = (angle/(PI/3));
+				float offset = 1-fmod(angle, PI/3)/(PI/3);
+				float brightness = 1-sqrt(((i-256)*(i-256)+(j-256)*(j-256)-50*50)/(float)(256*256));
+				serpiw[i][j][0] = (reds[index]*offset + reds[index+1]*(1-offset))+brightness;
+				serpiw[i][j][1] = (greens[index]*offset + greens[index+1]*(1-offset))+brightness;
+				serpiw[i][j][2] = (blues[index]*offset + blues[index+1]*(1-offset))+brightness;
+				int k;
+				for(k=0;k<3;k++)if(brightness>0.5)serpiw[i][j][k]+=brightness*2-1;else serpiw[i][j][k]*=brightness*2;
+			}
+			else if(i < 100)
+			{
+				serpiw[i][j][3] = 1;
+				serpiw[i][j][0]=1;
+				serpiw[i][j][1]=1;
+				serpiw[i][j][2]=1;
+			}
+			else if(i > 512-100)
+			{
+				serpiw[i][j][3] = 1;
+				serpiw[i][j][0]=0;
+				serpiw[i][j][1]=0;
+				serpiw[i][j][2]=0;
+			}
+			else if(j > 256+100 || j < 256-100 )
+			{
+				serpiw[i][j][3] = 1;
+				serpiw[i][j][0]=0.5;
+				serpiw[i][j][1]=0.5;
+				serpiw[i][j][2]=0.5;
+			}
+		}
+	}
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, 512, 512, 0, GL_RGBA, GL_FLOAT, serpiw);
 
+	int freevram;
+	glGetIntegerv(0x9049, &freevram);
+	printf("%dkb free vram\n", freevram);
 }
 
 void c_draw()
 {
-	static int sw =0;
-	sw+= 0xffffff;
-	target(layers[sw<0?1:0].texid);
-	drawlayer(sw<0?0:1);
+//	target(layers[1].texid);
+//	drawlayer(0);
 	untarget();
 	int i;
 	for(i=0;i<NUMLAYERS;i++)
 		drawlayer(i);
 	GLuint err = glGetError();
+	if(renderwheel)
+		drawwheel();
 	if(err != 0)
 		printf("gl error %d\n", err);
+}
+
+void c_selectlayer(int lay)
+{
+	currentlayer = lay;
+}
+void c_windowsize(float x, float y)
+{
+	windoww = x;
+	windowh = y;
+	windowa = y/x;
+}
+void b_color(float r, float g, float b, float a)
+{
+	brushes[0].r = r;
+	brushes[0].g = g;
+	brushes[0].b = b;
+	brushes[0].a = a;
+}
+void c_zoom(float zto)
+{
+//	panx = zoom*panx/zto;
+//	pany = zoom*pany/zto;
+	zoom = zto;
+}
+void c_showwheel(float x, float y)
+{
+	renderwheel = 1;
+	wheelx = x*2-1;
+	wheely = -(y*2-1);
+}
+void c_hidewheel()
+{
+	renderwheel = 0;
+}
+void c_translate(float dx, float dy)
+{
+	panx += dx*2;
+	pany -= dy*2;
+}
+
+void c_paint(float x, float y, float pressure)
+{
+
+	wtos(&x, &y);
+	target(layers[currentlayer].texid);
+	glBindTexture(GL_TEXTURE_2D,layers[1].texid);
+	glBindTexture(GL_TEXTURE_2D,0);//disable texturing
+//	glDisable(GL_TEXTURE_2D);
+	glBegin(GL_TRIANGLE_FAN);
+	glTexCoord2f(0.5,0.5);
+	glColor4f(brushes[selectedbrush].r, brushes[selectedbrush].g,brushes[selectedbrush].b, pressure*brushes[selectedbrush].a);
+	glVertex3f(x     , y, 0);
+	if(brushes[selectedbrush].softedge)
+		glColor4f(brushes[selectedbrush].r, brushes[selectedbrush].g,brushes[selectedbrush].b, 0);
+	int j;
+	for(j=0;j<51;j++)
+	{
+		glTexCoord2f(sin(j*PI/25)*0.5+0.5     , cos(j*PI/25)*0.5+0.5);
+		//glVertex3f(x  +0.01*sin(j*3.14/5)     , y + 0.01*cos(j*3.14/5), 0);
+		//glVertex3f(x  +0.05*sin(j*3.14/5)*pressure     , y + 0.05*cos(j*3.14/5)*pressure, 0);
+		glVertex3f(x  +0.1*sin(j*PI/25)*pressure/zoom     , y + 0.1*cos(j*PI/25)*pressure/zoom, 0);
+	}
+
+	glEnd();
+	glEnable(GL_TEXTURE_2D);
+	untarget();
+}
+void c_viscursor(float x, float y )
+{
+
+//	wtos(&x, &y);
+	x = (x*2)-1;
+	y = (y*-2)+1;
+	glBindTexture(GL_TEXTURE_2D,0);//disable texturing
+	glBegin(GL_LINE_STRIP);
+	glColor4f(brushes[selectedbrush].r, brushes[selectedbrush].g,brushes[selectedbrush].b, 1);
+	int j;
+	for(j=0;j<11;j++)
+	{
+		glTexCoord2f(sin(j*PI/5)*0.5+0.5     , cos(j*PI/5)*0.5+0.5);
+		glVertex3f(x  +0.1*sin(j*PI/5)*windowa, y + 0.1*cos(j*PI/5), 0);
+	}
+
+	glEnd();
+	glEnable(GL_TEXTURE_2D);
+}
+void c_droppersample(float x, float y)
+{
+	float retrieved[4*4];
+printf("windoww %f windowh %f", windoww, windowh);
+	glReadPixels(x*windoww, (1-y)*windowh, 2,2,GL_RGBA, GL_FLOAT, retrieved);
+	retrieved[0] = (retrieved[0]+retrieved[4]+retrieved[8]+retrieved[12])/4;
+	retrieved[1] = (retrieved[1]+retrieved[5]+retrieved[9]+retrieved[13])/4;
+	retrieved[2] = (retrieved[2]+retrieved[6]+retrieved[10]+retrieved[14])/4;
+
+
+	brushes[selectedbrush].r = retrieved[0];
+	brushes[selectedbrush].g = retrieved[1];
+	brushes[selectedbrush].b = retrieved[2];
+	brushes[selectedbrush].a = 1;//retrieved[3];
+//printf("sampled\n");
 }
 
--- external.c
+++ external.c
@@ -10,6 +10,8 @@
 
 #include "compositor.c"
 static float cursorx, cursory, pressure;
+static float precursorx, precursory, prepressure;
+static float zoom;
 
 static gboolean cbredraw(GtkWidget *da, GdkEventExpose *event, gpointer user_data)
 {
@@ -23,15 +25,19 @@ static gboolean cbredraw(GtkWidget *da, GdkEventExpose *event, gpointer user_dat
 		g_assert_not_reached ();
 	}
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-//do shit here
+		//glVertex3f(2*x/gdk_window_get_width(da->window)-1  +0.1*sin(j*3.14/5)     , 1-2*y/gdk_window_get_height(da->window) + 0.1*cos(j*3.14/5), 0);
+	//do shit here
+	c_windowsize(gdk_window_get_width(da->window), gdk_window_get_height(da->window));
+	
 	c_draw();
-
+	c_viscursor(cursorx/gdk_window_get_width(da->window), cursory/gdk_window_get_height(da->window));
+//	c_viswheel();
 	gdk_gl_drawable_gl_end (gldrawable);
-        if (gdk_gl_drawable_is_double_buffered (gldrawable))
-                gdk_gl_drawable_swap_buffers (gldrawable);
+	if (gdk_gl_drawable_is_double_buffered (gldrawable))
+		gdk_gl_drawable_swap_buffers (gldrawable);
 
-        else
-                glFlush ();
+	else
+		glFlush ();
 
 
 	return TRUE;
@@ -40,7 +46,7 @@ static gboolean cbredraw(GtkWidget *da, GdkEventExpose *event, gpointer user_dat
 
 static gboolean cbconfigure(GtkWidget *da, GdkEventConfigure *event, gpointer user_data)
 {
-//(from example)
+	//(from example)
 	GdkGLContext *glcontext = gtk_widget_get_gl_context (da);
 	GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (da);
 
@@ -54,7 +60,7 @@ static gboolean cbconfigure(GtkWidget *da, GdkEventConfigure *event, gpointer us
 	//glOrtho (-10,10,-10,10,-20050,10000);
 	glOrtho (-1,1,-1,1,-20050,10000);
 	gdk_gl_drawable_gl_end (gldrawable);
-//(/from example)
+	//(/from example)
 }
 static gboolean  mousemove(GtkWidget *widget, GdkEventMotion *event)
 {
@@ -63,33 +69,176 @@ static gboolean  mousemove(GtkWidget *widget, GdkEventMotion *event)
 	gdk_device_get_state( event->device,event->window, stuff, NULL);
 	int i;
 	//if(stuff[2] > 0)
-		//printf("pressure:%f \n",stuff[2]);
 	cursorx = stuff[0];
 	cursory = stuff[1];
 	pressure = stuff[2];
 	//gdk_input_window_get_pointer(event->window, event->device, NULL, NULL, &pressure, NULL,NULL,NULL);
-	gdk_window_invalidate_rect (event->window, &widget->allocation, FALSE);
+
+
+
+
+
+
+
+
+
+
+
+	GdkGLContext *glcontext = gtk_widget_get_gl_context (widget);
+	GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (widget);
+
+	// g_print (" :: expose\n");
+
+	if (!gdk_gl_drawable_gl_begin (gldrawable, glcontext))
+	{
+		g_assert_not_reached ();
+	}
+	//do shit here
+	c_windowsize(gdk_window_get_width(widget->window), gdk_window_get_height(widget->window));
+	if(pressure > 0)
+	{
+		int i;
+		int numsamples = ceil(sqrt((cursorx - precursorx)*(cursorx - precursorx) + (cursory - precursory)* (cursory - precursory) )*0.5);
+		//printf("texid%d\n", texid);
+		for(i=0;i<numsamples;i++)
+		{
+			float cx = (cursorx * i + precursorx * (numsamples-i) )/ numsamples;
+			float cy = (cursory * i + precursory * (numsamples-i) )/ numsamples;
+			float p = 0.01+(pressure * i + prepressure * (numsamples-i) )/ numsamples;
+			c_paint(cx/gdk_window_get_width(widget->window),cy/gdk_window_get_height(widget->window),p*p);
+		}
+
+	}
+
+	precursorx = cursorx;
+	precursory = cursory;
+	prepressure = pressure;
+
+	gdk_gl_drawable_gl_end (gldrawable);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	gdk_window_invalidate_rect (event->window, NULL, TRUE);
 	return 1;
 
+}
+static gboolean cbscroll(GtkWidget *widget, GdkEventScroll *event, gpointer notused)
+{
+	if(event->direction == GDK_SCROLL_UP)
+		zoom /= 0.9;
+	else if(event->direction == GDK_SCROLL_DOWN)
+		zoom *= 0.9;
+	zoom = zoom>1000?1000:zoom;
+	zoom = zoom<1?1:zoom;
+	c_zoom(zoom);
+	gdk_window_invalidate_rect (event->window, NULL, TRUE);
+	return FALSE;
+
+}
+static gboolean cbclicked(GtkWidget *widget, GdkEventButton *event, gpointer notused)
+{
+printf("clicked %d\n", event->button);
+	float rx, ry;
+	static float dragtx, dragty;
+	if(event->axes ==NULL)
+	{
+		rx = event->x;
+		ry = event->y;
+		printf("x:%f\n", rx);
+	}
+	else
+	{printf("derp\n");
+		rx = cursorx;//event->axes[0];
+		ry = cursory;//event->axes[1];
+		printf("x:%f\n", rx);
+		printf("y:%f\n", ry);
+		printf("x:%d\n", gdk_window_get_width(widget->window));
+	}
+
+
+	if(event->button == 3)
+	{
+		if(event->type == GDK_BUTTON_PRESS)
+			c_showwheel(rx/gdk_window_get_width(widget->window),ry/gdk_window_get_height(widget->window));
+		else //if(event->type == GDK_BUTTON_RELEASE)
+		{
+			c_hidewheel();
+			c_droppersample(rx/gdk_window_get_width(widget->window), ry/gdk_window_get_height(widget->window));
+		}
+	}
+	else if(event->button == 4)
+		zoom /= 0.9;
+	else if(event->button == 5)
+		zoom *= 0.9;
+	else if(event->button == 2)
+	{
+		if(event->type == GDK_BUTTON_PRESS)
+		{
+			dragtx = rx/gdk_window_get_width(widget->window);
+			dragty = ry/gdk_window_get_height(widget->window);
+		}
+		else
+		{
+			dragtx = rx/gdk_window_get_width(widget->window) - dragtx;
+			dragty = ry/gdk_window_get_height(widget->window) - dragty;
+			c_translate(dragtx, dragty);
+			printf("translate %f %f\n", dragtx, dragty);
+
+		}
+	}
+	zoom = zoom>1000?1000:zoom;
+	zoom = zoom<1?1:zoom;
+	c_zoom(zoom);
+
+
+	gdk_window_invalidate_rect (event->window, NULL, TRUE);
+	return FALSE;
 }
 int main( int  argc, char **argv)
 {
 	GtkWidget *window;
 	gtk_init(&argc, &argv);
 	gtk_gl_init(&argc, &argv);
-//(from example)
+	//(from example)
 	GtkWidget *da;
 	GdkGLConfig *glconfig;
 
-	gtk_init (&argc, &argv);
-	gtk_gl_init (&argc, &argv);
+/*
+	GtkBuilder *build = gtk_builder_new();
+	gtk_builder_add_from_file(build, "window.builder", NULL);
+	da = GTK_WIDGET(gtk_builder_get_object(build, "da"));
+	window = GTK_WIDGET(gtk_builder_get_object(build, "win"));
+	gtk_builder_connect_signals(build, NULL);
+*/
 
 
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
 	da = gtk_drawing_area_new ();
 
-	gtk_container_add (GTK_CONTAINER (window), da);
+	GtkWidget *pane = gtk_hpaned_new();
+	gtk_container_add (GTK_CONTAINER (window), pane);
+
+	GtkWidget *button = gtk_button_new();
+	gtk_container_add(GTK_CONTAINER(pane), button);
+
+
+	gtk_container_add (GTK_CONTAINER (pane), da);
 	g_signal_connect_swapped (window, "destroy",
 			G_CALLBACK (gtk_main_quit), NULL);
 	gtk_widget_set_events (da, GDK_EXPOSURE_MASK);
@@ -99,7 +248,7 @@ int main( int  argc, char **argv)
 	/* prepare GL */
 	glconfig = gdk_gl_config_new_by_mode (
 			GDK_GL_MODE_RGB |
-			GDK_GL_MODE_DEPTH |
+			//		GDK_GL_MODE_DEPTH |
 			GDK_GL_MODE_DOUBLE);
 
 	if (!glconfig)
@@ -119,7 +268,8 @@ int main( int  argc, char **argv)
 			G_CALLBACK (cbredraw), NULL);
 
 	gtk_widget_show_all (window);
-//(/from example)
+	gtk_window_fullscreen(GTK_WINDOW(window));
+	//(/from example)
 
 
 
@@ -127,15 +277,23 @@ int main( int  argc, char **argv)
 	GList *devices = gdk_devices_list();
 	while(devices->next != NULL)
 	{
-		printf("found input device\n");
-		if(!gdk_device_set_mode(devices->data, GDK_MODE_SCREEN))
-			printf("failed to enable\n");
+		int numaxes = gdk_device_get_n_axes(devices->data);
+		printf("found input device %i axes\n", numaxes);
+		if(numaxes > 2)
+		{
+			if(!gdk_device_set_mode(devices->data, GDK_MODE_SCREEN))
+				printf("failed to enable\n");
+		}
 		devices = devices->next;
 	}
-	g_signal_connect(window, "motion-notify-event", G_CALLBACK(mousemove), NULL);
-
-	gdk_input_set_extension_events(window->window, 0xffffffff, GDK_EXTENSION_EVENTS_ALL);
+	gtk_widget_add_events(GTK_WIDGET(da), GDK_SCROLL_MASK|GDK_BUTTON_PRESS_MASK);
+	g_signal_connect(da, "motion-notify-event", G_CALLBACK(mousemove), NULL);
+	g_signal_connect (da, "scroll-event",G_CALLBACK (cbscroll), NULL);//FIXME:doesn't work with extension events enabled
+	g_signal_connect (da, "button-press-event",G_CALLBACK (cbclicked), NULL);//FIXME:doesn't work with extension events enabled
+	g_signal_connect (da, "button-release-event",G_CALLBACK (cbclicked), NULL);//FIXME:doesn't work with extension events enabled
+	gdk_input_set_extension_events(da->window, 0xffffffff, GDK_EXTENSION_EVENTS_ALL);
 
 	c_init();
+	b_color(0.5,0.5,1,0.1);
 	gtk_main();
 }
